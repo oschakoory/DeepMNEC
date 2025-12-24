c108c132c1676ce67f851fbd1aa94c131934f6fc
Oshma <62591558+oschakoory@users.noreply.github.com>
2025-12-24 07:57:56 +0100

    Fix index for shap_values in waterfall plot

--- dashboard.py
+++ dashboard.py
@@ -243,7 +243,7 @@ def my_prediction(unknown, new_samp):
                     #feature_names=test_sample_t.columns,
                     #plot_type="bar", show=False))
     
-    st_shap(waterfall(Explanation(shap_values[1][0], 
+    st_shap(waterfall(Explanation(shap_values[0][1],
         explainer.expected_value[1], 
         data=test_sample_t.iloc[0], 
         feature_names=test_sample_t.columns)))
